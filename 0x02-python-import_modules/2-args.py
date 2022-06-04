36bf166cd1c9b4ef548fd71b2de8ea45504d3090
AJIyanu <aderemijoseph@live.com>
2022-06-04 20:21:20 +0100

    arguementative essay

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+
+n = len(sys.argv)
+if __name__ == "__main__":
+    if n == 1:
+        print("{} arguements.".format(n - 1))
+    elif n < 3:
+        print("{} arguement:".format(n - 1))
+    else:
+        print("{} arguements:".format(n - 1))
+    for i in range(1, n):
+        print("{}:".format(i), sys.argv[i])
