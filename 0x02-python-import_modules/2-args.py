71314afd75d7777677340fc43a657205b4c907c7
AJIyanu <aderemijoseph@live.com>
2022-06-04 20:29:44 +0100

    argument not arguement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ import sys
 n = len(sys.argv)
 if __name__ == "__main__":
     if n == 1:
-        print("{} arguements.".format(n - 1))
+        print("{} arguments.".format(n - 1))
     elif n < 3:
-        print("{} arguement:".format(n - 1))
+        print("{} argument:".format(n - 1))
     else:
-        print("{} arguements:".format(n - 1))
+        print("{} arguments:".format(n - 1))
     for i in range(1, n):
-        print("{}: ".format(i), sys.argv[i])
+        print("{}".format(i), sys.argv[i], sep=': ')
