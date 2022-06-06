a8fe59df40fbe2fb5e0afdc435de570aa19324fa
AJIyanu <aderemijoseph@live.com>
2022-06-06 08:23:20 +0100

    criteria

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import hidden_4
-names = dir(hidden_4)
-n = len(names)
-for i in range(n):
-    ch = names[i][0]
-    if ch != '_':
-        print(names[i])
+if __name__ == "__main__":
+    names = dir(hidden_4)
+    n = len(names)
+    for i in range(n):
+        ch = names[i][0]
+        if ch != '_':
+            print(names[i])
