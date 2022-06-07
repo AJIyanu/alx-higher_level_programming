aaa76157dede0a715a5129a9d35ed5bff75aa38a
AJIyanu <aderemijoseph@live.com>
2022-06-07 21:45:40 +0100

    progresss?!

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,13 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[] - 1):
-                print("{:d}".format(matrix[i][j]), end=" ")
-                print("{:d}".format(matrix[i][j + 1]))
+    row = len(matrix)
+    column = len(matrix[0])
+    col = column - 1
+    if column > 0 and row > 0:
+        for i in range(row):
+            for j in range(col):
+                print("{:d}".format(matrix[i][j]), end=' ')
+            print("{:d}".format(matrix[i][col]))
+        else:
+            print()
