71d90e6df76427a764d00813c7712063f9ddfada
AJIyanu <aderemijoseph@live.com>
2022-06-07 09:55:42 +0100

    new list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx < 0:
-        return (my_list)
-    if idx > len(my_list) - 1:
-        return (my_list)
     my_list1 = my_list
+    if idx < 0:
+        return (my_list1)
+    if idx > len(my_list) - 1:
+        return (my_list1)
     my_list1[idx] = element
     return (my_listi1)
