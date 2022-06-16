7cf9f6bf3ac3135e874129f184e3bf936cdea6e1
AJIyanu <aderemijoseph@live.com>
2022-06-16 18:42:55 +0100

    set is diff from list

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,10 +2,10 @@
 
 
 def common_elements(set_1, set_2):
-    common = []
-    for a in range(len(set_1)):
-        for j in range(len(set_2)):
-                if set_2[j] == set_1[a]:
-                    common.append(set_1[a])
+    common = {*()}
+    for a in set_1:
+        for j in set_2:
+                if a == j:
+                   common.add(j)
     return (common)
 
