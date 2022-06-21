b47e30135006b342a762814ab98b187a91ed288b
AJIyanu <aderemijoseph@live.com>
2022-06-21 06:55:38 +0100

    not handled index

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,13 +3,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
+    for i in range(x):
+        try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
-        print()
-        return (count)
-    except TypeError:
-        pass
-    except ValueError:
-        pass
+        except TypeError:
+            continue
+        except ValueError:
+            continue
+    print()
+    return (count)
