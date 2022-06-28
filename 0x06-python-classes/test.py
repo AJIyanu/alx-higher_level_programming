8d6487af48c08f94de18f861509bd701d44621ab
AJIyanu <aderemijoseph@live.com>
2022-06-28 16:50:52 +0100

    how far

--- 0x06-python-classes/test.py
+++ 0x06-python-classes/test.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-Square = __import__('6-square').Square
+Square = __import__('101-square').Square
 
-try:
-    my_square = Square(3, (1, "3"))
-except Exception as e:
-    print(e)
+my_square = Square(5, (0, 0))
+print(my_square)
+
+print("--")
+
+my_square = Square(5, (4, 1))
+print(my_square)
