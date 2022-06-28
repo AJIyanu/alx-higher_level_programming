5efc03028ec82f0bf190018ee199b87b2d4d1003
AJIyanu <aderemijoseph@live.com>
2022-06-28 02:58:26 +0100

    using self but not selfish

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+"""
+This time we are writing a code with validation and all positives
+"""
+
+
+class Square(object):
+    """
+    This is a class that defines the sqaure and its attributes
+    """
+    def __init__(self, size=0):
+        """
+        This is the initializing point that takes the value size
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        """
+        This module computes and returns the area of a square
+        """
+        return (self._Square__size ** 2)
