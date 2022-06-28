aba232af2494743ea58a2b61ef2a49ca2c130e20
AJIyanu <aderemijoseph@live.com>
2022-06-28 02:30:57 +0100

    value validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+"""
+Tbis time we are writing a code with validation and all positives
+"""
+
+
+class Square(object):
+     """
+     This is a class that defines the sqaure and its attributes
+     """
+     def __init__(self, size=0):
+         """
+         This is the initializing point that takes the value size
+         """
+         if type(size) is not int:
+             raise TypeError("size must be an integer")
+         if size < 0:
+             raise ValueError("size must be >= 0")
+         self._Square__size = size
