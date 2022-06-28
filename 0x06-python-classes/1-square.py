deebe6decebdfed9f0ccb31834b07210b8bd89f1
AJIyanu <aderemijoseph@live.com>
2022-06-28 01:46:07 +0100

    lati ijo yii"

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""
+This modeule teaches private instance
+"""
+
+
+class Square(object):
+    """
+    This class defines sqaure. this has a private instance
+    """
+
+    def __init__(self, size):
+        """
+        This is a private instance that desxibes size
+        """
+        self._Square__size = size
