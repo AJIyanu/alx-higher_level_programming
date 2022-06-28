17df6f95871f6cc0c49fd36ace9ed92f24b986d1
AJIyanu <aderemijoseph@live.com>
2022-06-28 02:37:14 +0100

    validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -2,20 +2,20 @@
 
 
 """
-Tbis time we are writing a code with validation and all positives
+This time we are writing a code with validation and all positives
 """
 
 
 class Square(object):
-     """
-     This is a class that defines the sqaure and its attributes
-     """
-     def __init__(self, size=0):
-         """
-         This is the initializing point that takes the value size
-         """
-         if type(size) is not int:
-             raise TypeError("size must be an integer")
-         if size < 0:
-             raise ValueError("size must be >= 0")
-         self._Square__size = size
+    """
+    This is a class that defines the sqaure and its attributes
+    """
+    def __init__(self, size=0):
+        """
+        This is the initializing point that takes the value size
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
