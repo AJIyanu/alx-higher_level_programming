8e71e0dfaefb78eab1d53153612d7a7fe1b11c2c
AJIyanu <aderemijoseph@live.com>
2022-06-29 10:55:47 +0100

    empty class.

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+"""
+This module describes a rectangle and all its properties
+stay tuned for developments
+"""
+
+
+class Rectangle:
+    """
+    This is the class rectangle that I told you about
+    """
+    pass
