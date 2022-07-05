db48cd81b99146df8dca71fb40c72b076da94f7b
AJIyanu <aderemijoseph@live.com>
2022-07-05 22:33:24 +0100

    which requirement is missing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,12 +2,17 @@
 """
 This module describe writinng to a file
 method or function
+what is goin on
+modules up to
+just about five lines
 """
+
 
 def read_file(filename=""):
     """
     This function reads the content of a file
     and prints to stdoutput
+    here
     """
     file = open(filename)
     print(file.read())
