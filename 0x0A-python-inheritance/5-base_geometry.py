11d3acb7ca009e12046713be1e3b7ed17f7ebee7
AJIyanu <aderemijoseph@live.com>
2022-07-05 03:23:32 +0100

    empty vlass

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+"""
+This is a module
+It is a base geometry one
+For now it is empty
+stay tuned while we work on filling it up
+oh yeah
+"""
+
+
+class BaseGeometry(object):
+    """
+    This is a class inherited from object
+    This is empty now
+    pass
+    """
+    pass
