d96b594705465da1ed68c65bcabcb14a0b035ca9
AJIyanu <aderemijoseph@live.com>
2022-07-06 16:45:08 +0100

    json file pointer

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,6 +11,6 @@ def load_from_json_file(filename):
     """
     We load from json file
     """
-    with open(filename, 'w+', encoding="UTF-8") as file:
+    with open(filename, 'r', encoding="UTF-8") as file:
         obj = json.load(file)
     return (obj)
