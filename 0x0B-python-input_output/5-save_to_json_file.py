48424623ea3bf3444f9d04f60dd215adc3027201
AJIyanu <aderemijoseph@live.com>
2022-07-06 16:35:16 +0100

    json file pointer

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+"""
+Jason to be in file
+"""
+
+
+json = __import__("json")
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    saving to jsonfile not string
+    """
+    with open(filename, 'w', encoding="UTF-8") as file:
+        json.dump(my_obj, file)
+
