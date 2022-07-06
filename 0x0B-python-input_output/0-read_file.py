11d9b6c294767fa96c84a1b4e4bce8912102df01
AJIyanu <aderemijoseph@live.com>
2022-07-06 01:31:12 +0100

    one mind

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -14,5 +14,5 @@ def read_file(filename=""):
     and prints to stdoutput
     here
     """
-    with open(filename, encoding="utf-8") as file:
+    with open(filename, encoding="UTF8") as file:
         print(file.read())
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This moduke writes to a file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Thiz function opens the file, and writr to it
+    """
+    with open(filename, 'r+', encoding="UTF8") as f:
+        count = f.write(text)
+    return (count)
+
