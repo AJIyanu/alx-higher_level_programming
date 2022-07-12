203ecae2c43cc10be0f4ec6f91208c39ec99fbfb
AJIyanu <aderemijoseph@live.com>
2022-07-12 17:39:13 +0100

    more comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -3,6 +3,8 @@
 This is a Rectangle modeule
 It will inherit the base module for id
 It will also have its own methods and stuffs
+Let us make this
+Five lines
 """
 
 
@@ -12,11 +14,18 @@ from base import Base
 class Rectangle(Base):
     """
     Rectangle class with initializer
+    Then we add more lines here
+    and here also
+    and here also
+    and here also
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
         This initializer inherits the super class
+        This too should probably increase
+        to thiz too
+        and here too
         """
         super().__init__
         self.__width = width
