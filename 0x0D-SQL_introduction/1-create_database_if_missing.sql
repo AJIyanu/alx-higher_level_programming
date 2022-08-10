e86d63174e529384e4826ead127b7a21bc9b576b
AJIyanu <aderemijoseph@live.com>
2022-08-10 16:42:57 +0100

    to be continued

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,6 @@
 -- This is a comment
 -- create database if not exist condition included
 
-SHOW CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0 
+CREATE DATABASE [IF NOT EXISTS] hbtn_0c_0
+[CHARACTER SET charset_name]
+[COLLATE collation_name]
