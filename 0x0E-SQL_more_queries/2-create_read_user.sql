67f16939011b2faaff82f06665c8d704482da05f
AJIyanu <aderemijoseph@live.com>
2022-08-11 17:53:59 +0100

    select priviledges

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,9 +4,8 @@
 
 
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
 
 CREATE SCHEMA IF NOT EXISTS hbtn_0d_2;
 
 GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
