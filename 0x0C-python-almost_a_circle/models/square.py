4b06f5b3192c5e996a42bf66787fcc204fc11689
AJIyanu <aderemijoseph@live.com>
2022-08-18 23:58:40 +0100

    Commentary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,6 +2,7 @@
 """
 This is a square module
 It is a lot easier to program with vscode
+I am just going to increase comment
 """
 
 from .rectangle import Rectangle
@@ -10,18 +11,24 @@ from .rectangle import Rectangle
 class Square(Rectangle):
     """
     This is a square class inheriting a rextangle class
+    This is a square class inheriting a rextangle class
+    This is a square class inheriting a rextangle class
+    This is a square class inheriting a rextangle class
+    This is a square class inheriting a rextangle class
     """
 
     def __init__(self, size, x=0, y=0, id=None):
         """
-        Ghis is the constructor here...
+        This is the constructor here...
         nothing special just using super class
+        Comment
         """
         super(Square, self).__init__(size, size, x, y, id)
 
     def __repr__(self):
         """
         Overider represent of string
+        Has a comment too
         """
         tt = self.id
         ty = super().x
