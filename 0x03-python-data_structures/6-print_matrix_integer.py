9aefac9cf0754161d52d53fda26df19e12e5ef1f
AJIyanu <aderemijoseph@live.com>
2022-08-29 22:24:59 +0100

    what to do

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,13 +2,12 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    row = len(matrix)
-    column = len(matrix[0])
-    col = column - 1
-    if column > 0 and row > 0:
-        for i in range(row):
-            for j in range(col):
-                print("{:d}".format(matrix[i][j]), end=' ')
-            print("{:d}".format(matrix[i][col]))
-        else:
-            print()
+    if matrix is None:
+        return
+    if len(matrix[0]) == 0:
+        print()
+        return
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0]) - 1):
+            print("{} ".format(matrix[i][j]), end='')
+        print("{}".format(matrix[i][len(matrix[0]) - 1]))
