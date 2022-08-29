45f4d740565fa63833c91b4a657ac524c49e9c5c
AJIyanu <aderemijoseph@live.com>
2022-08-29 22:36:39 +0100

    super pet

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,10 +4,10 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix is None:
         return
-    if len(matrix[0]) == 0:
-        print()
-        return
     for i in range(len(matrix)):
         for j in range(len(matrix[0]) - 1):
             print("{} ".format(matrix[i][j]), end='')
+        if len(matrix[0]) == 0:
+            print()
+            return
         print("{}".format(matrix[i][len(matrix[0]) - 1]))
