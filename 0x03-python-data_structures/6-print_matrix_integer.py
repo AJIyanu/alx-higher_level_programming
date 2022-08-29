5813dad55b5d861bdbe37b89c0bff1d0498aa098
AJIyanu <aderemijoseph@live.com>
2022-08-29 22:38:07 +0100

    super pet

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,8 +6,8 @@ def print_matrix_integer(matrix=[[]]):
         return
     for i in range(len(matrix)):
         for j in range(len(matrix[0]) - 1):
-            print("{} ".format(matrix[i][j]), end='')
+            print("{:d} ".format(matrix[i][j]), end='')
         if len(matrix[0]) == 0:
             print()
             return
-        print("{}".format(matrix[i][len(matrix[0]) - 1]))
+        print("{:d}".format(matrix[i][len(matrix[0]) - 1]))
