1c67e96f298c6803a4936c7dfb84bd1fcb56c8da
AJIyanu <aderemijoseph@live.com>
2022-09-07 13:16:36 +0100

    add function

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const arg = process.argv;
+const int1 = parseInt(arg[2]);
+const int2 = parseInt(arg[3]);
+function add (a, b) {
+  if (!isNaN(a) && (!isNaN(b))) {
+    return (a + b);
+  }
+}
+console.log(add(int1, int2));
