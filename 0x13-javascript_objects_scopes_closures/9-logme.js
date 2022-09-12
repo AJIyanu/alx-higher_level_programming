cbb191a5fb3257ae9e03a555034b9e691881e888
AJIyanu <aderemijoseph@live.com>
2022-09-12 13:17:41 +0100

    fixed outside not inside funtion

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,11 +1,7 @@
-#!!/usr/bin/node
+#!/usr/bin/node
 
 let count = 0;
 exports.logMe = function (item) {
-  function dispcount (itemm) {
-    console.log(count + ' : ' + itemm);
-    count += 1;
-  }
-
-  return (dispcount(item));
+  console.log(count + ' : ' + item);
+  count += 1;
 };
