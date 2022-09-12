79c34acff64ff0b2df252eb32efaba4f0bbe3a95
AJIyanu <aderemijoseph@live.com>
2022-09-12 04:58:35 +0100

    docs

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""This is a database python module
+"""
 
 import MySQLdb
 import sys
@@ -7,10 +8,11 @@ import pymysql
 pymysql.install_as_MySQLdb()
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
+    """This to make sure code is not jmported"""
     db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states") 
+    cursor.execute("SELECT * FROM states")
     result = cursor.fetchall()
     for row in result:
         print(row)
