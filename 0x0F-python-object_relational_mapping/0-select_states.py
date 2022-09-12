74f40e7126b13775e8b9f0fe77bd9958c7bf368a
AJIyanu <aderemijoseph@live.com>
2022-09-12 13:06:30 +0100

    no execute

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ pymysql.install_as_MySQLdb()
 if __name__ == "__main__":
     db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states")
+   # cursor.execute("SELECT * FROM states")
     result = cursor.fetchall()
     for row in result:
         print(row)
