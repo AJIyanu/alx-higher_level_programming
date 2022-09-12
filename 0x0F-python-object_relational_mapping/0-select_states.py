e41b9b214cd15c5bdc0a1879a08a19bb34fbcc26
AJIyanu <aderemijoseph@live.com>
2022-09-12 07:58:34 +0100

    more document

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,10 +14,6 @@ pymysql.install_as_MySQLdb()
 
 
 if __name__ == "__main__":
-    """This to make sure code is not jmported
-    enough documents added
-    more enough
-    """
     db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states")
