4963227d7522f74ff4c2ebe8b97eca61b3db2565
AJIyanu <aderemijoseph@live.com>
2022-09-12 21:22:50 +0100

    tor

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
                 user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
                 )
         c = db_connection.cursor()
-        c.execute("SELECT * FROM states WHERE name like 'N%' ORDER  BY id ASC;")
+        c.execute("SELECT * FROM states WHERE name like 'N%' \
+			ORDER  BY id ASC;")
         results = c.fetchall()
         for result in results:
-			if result[1][0] == "N":
-            	print(result)
+            print(result)
         db_connection.close
