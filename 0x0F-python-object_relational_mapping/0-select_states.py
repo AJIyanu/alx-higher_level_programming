437a6d1e52424a5a7869afd7291726764e493885
AJIyanu <aderemijoseph@live.com>
2022-09-12 13:08:02 +0100

    indentation problem

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3 
- """ 
- This module lists all state from database
- documennted right? 
- """ 
+""" 
+This module lists all state from database
+documennted right? 
+""" 
   
  
 import MySQLdb
@@ -14,7 +14,8 @@ pymysql.install_as_MySQLdb()
 if __name__ == "__main__":
     db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
     cursor = db.cursor()
-   # cursor.execute("SELECT * FROM states")
+    cursor.execute("SELECT * FROM states")
+    
     result = cursor.fetchall()
     for row in result:
         print(row)
