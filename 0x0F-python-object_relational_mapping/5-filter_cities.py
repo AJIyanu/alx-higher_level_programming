9bcf59bd6ecf749b7b0adad8a7e84743fcf34b0f
AJIyanu <aderemijoseph@live.com>
2022-09-13 03:57:21 +0100

    compile

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,7 +5,7 @@ python script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-argn = sys.argv[4]
+#argn = sys.argv[4]
 #safe_name = argn.replace("'", "")
 
 if __name__ == "__main__":
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                 )
         c = db_connection.cursor()
         c.execute("SELECT c.name FROM cities as c \
-INNER JOIN states as s ON c.state_id = s.id WHERE s.id LIKE '{}' ORDER  BY c.id;".format(argn))
+INNER JOIN states as s ON c.state_id = s.id WHERE s.id LIKE '{}' ORDER  BY c.id;".format(sys.argv[4]))
         results = c.fetchall()
         for result in results:
             print(result)
