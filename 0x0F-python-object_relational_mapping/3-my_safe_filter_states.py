0019af9fb3358985d2faf1fe3fe41b90ca7c0d89
AJIyanu <aderemijoseph@live.com>
2022-09-13 04:40:00 +0100

    test 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,8 +5,6 @@ python script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-inputarg = sys.argv[4]
-safe_input = inputarg.replace("'", "")
 if __name__ == "__main__":
     if len(sys.argv) >= 4:
         db_connection = MySQLdb.connect(
@@ -14,6 +12,8 @@ if __name__ == "__main__":
                 user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
                 )
         c = db_connection.cursor()
+        inputarg = sys.argv[4]
+        safe_input = inputarg.replace("'", "")
         c.execute("SELECT * FROM states WHERE name LIKE '{}' \
 ORDER  BY id ASC;".format(safe_input))
         results = c.fetchall()
