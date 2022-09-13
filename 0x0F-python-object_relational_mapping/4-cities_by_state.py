ae4c117cf4ba64aaa0be8b758fe006326817a09e
AJIyanu <aderemijoseph@live.com>
2022-09-13 03:14:45 +0100

    spaced input query

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
                 user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
                 )
         c = db_connection.cursor()
-        c.execute("SELECT c.id, c.name, s.name FROM cities as c\
+        c.execute("SELECT c.id, c.name, s.name FROM cities as c \
 INNER JOIN states as s ON c.state_id = s.id ORDER  BY c.id;")
         results = c.fetchall()
         for result in results:
