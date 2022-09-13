9f82b72c0b2c40ce879a8cc8f5ce8d85dd667507
AJIyanu <aderemijoseph@live.com>
2022-09-13 04:08:45 +0100

    argv

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -5,8 +5,6 @@ python script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
-state_name = sys.argv[4]
-
 if __name__ == "__main__":
     if len(sys.argv) >= 5:
         db_connection = MySQLdb.connect(
@@ -14,7 +12,7 @@ if __name__ == "__main__":
                 user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
                 )
         c = db_connection.cursor()
-        c.execute("SELECT c.name FROM cities as c INNER JOIN states as s ON c.state_id = s.id WHERE s.name LIKE %s ORDER  BY c.id;", (state_name,))
+        c.execute("SELECT c.name FROM cities as c INNER JOIN states as s ON c.state_id = s.id WHERE s.name LIKE %s ORDER  BY c.id;", (sys.argv[4],))
         results = c.fetchall()
         for result in results:
             print(result[0])
