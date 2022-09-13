bfa06beaca0ec0ef65e21e9ea03a4d8ed0bf2c75
AJIyanu <aderemijoseph@live.com>
2022-09-13 03:55:38 +0100

    compile error?

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,7 @@ import MySQLdb
 import sys
 
 argn = sys.argv[4]
-safe_name = argn.replace("'", "")
+#safe_name = argn.replace("'", "")
 
 if __name__ == "__main__":
     if len(sys.argv) >= 4:
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                 )
         c = db_connection.cursor()
         c.execute("SELECT c.name FROM cities as c \
-INNER JOIN states as s ON c.state_id = s.id WHERE s.id LIKE '{}' ORDER  BY c.id;".format(safe_name))
+INNER JOIN states as s ON c.state_id = s.id WHERE s.id LIKE '{}' ORDER  BY c.id;".format(argn))
         results = c.fetchall()
         for result in results:
             print(result)
