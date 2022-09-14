d07383f902509b62703c3fda8aad5bf8bdcb827c
AJIyanu <aderemijoseph@live.com>
2022-09-14 07:52:48 -0700

    still learnng

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,7 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
-	"""This needs to be documented and it is now"""
+    """This needs to be documented and it is now"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name =  Column(String(128), nullable=False)
