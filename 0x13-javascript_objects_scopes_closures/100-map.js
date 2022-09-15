7d015e50530691fa25eafde4092b85dd4f62a2af
AJIyanu <aderemijoseph@live.com>
2022-09-15 17:19:16 +0100

    mo daa

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('100-data.js').list;
+const list = require('./100-data').list;
 console.log(list);
 const listnew = list.map(x => x * list.indexOf(x));
 console.log(listnew);
