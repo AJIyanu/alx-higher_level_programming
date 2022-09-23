10d2ae1bc51790c7abbf58b0c548293958569ac3
AJIyanu <aderemijoseph@live.com>
2022-09-23 14:25:39 +0100

    comment added and not importable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Trying to requesr url with urllib"""
 
-
-import urllib.request
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-	html = response.read()
-print("Body response:")
-print("\t- type: " + str(html))
-print("\t- content: " + str(type(html)))
-print("\t- utf8 content: " + html.decode("utf-8"))
+if __name__ == "__main__":
+	import urllib.request
+	with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+		html = response.read()
+	print("Body response:")
+	print("\t- type: " + str(html))
+	print("\t- content: " + str(type(html)))
+	print("\t- utf8 content: " + html.decode("utf-8"))
