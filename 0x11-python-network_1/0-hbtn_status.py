d4a740bf70187b5d3469c45dd0e65d927404589e
AJIyanu <aderemijoseph@live.com>
2022-09-23 10:54:58 -0700

    back to vs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """Trying to requesr url with urllib"""
 
+
 if __name__ == "__main__":
-	import urllib.request
-	with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-		html = response.read()
-	print("Body response:")
-	print("\t- type: " + str(html))
-	print("\t- content: " + str(type(html)))
-	print("\t- utf8 content: " + html.decode("utf-8"))
+    import urllib.request
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: " + str(html))
+        print("\t- content: " + str(type(html)))
+        print("\t- utf8 content: " + html.decode("utf-8"))
