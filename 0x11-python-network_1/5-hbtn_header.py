b2c7268a660e037cee1f8d4548eb10e85821e4cd
AJIyanu <aderemijoseph@live.com>
2022-09-24 06:57:48 +0100

    100%

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -6,4 +6,5 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     reply = requests.get(url)
-    print(reply.headers["X-Request-Id"])
+    if X-Request-Id in reply.headers:
+        print(reply.headers["X-Request-Id"])
