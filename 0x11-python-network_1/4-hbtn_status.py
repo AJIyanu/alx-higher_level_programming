4faf4fb6a48e507b86d1749bb1cb0cc18682f14d
AJIyanu <aderemijoseph@live.com>
2022-09-24 03:32:56 +0100

    request with request

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
-"""My Comments come here and must be edited"""
+"""Trying to request url with urllib"""
 
+import requests
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    reply = requests.get(url)
+    print("Body response:")
+    print("\t- type: " + str(type(reply.text)))
+    print("\t- content: " + str(reply.text))
