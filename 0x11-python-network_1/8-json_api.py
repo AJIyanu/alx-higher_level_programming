b41ba98f0edc2c1ea4a6348ac8265922d162f257
AJIyanu <aderemijoseph@live.com>
2022-09-24 04:48:15 +0100

    exempting json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,14 +5,16 @@ import requests
 import sys
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) > 0:
+    if len(sys.argv) > 1:
         post = {"q": sys.argv[1]}
     else:
         post = {"q": ""}
     reply = requests.post(url, post)
-    if reply.json():
+    try:
         json = reply.json()
         if len(json) != 0:
             print("[{}] {}".format(json['id'], json['name']))
         else:
             print("No result")
+    except:
+        print("Not a valid JSON")
