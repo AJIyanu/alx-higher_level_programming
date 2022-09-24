d8bf0ca8e809bebfd7f5e0129b4c79659d172940
AJIyanu <aderemijoseph@live.com>
2022-09-24 04:33:54 +0100

    updated

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,10 +5,11 @@ import requests
 import sys
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if sys.argv[1]:
+    if len(sys.arg) > 0:
         post = {"q": sys.argv[1]}
     else:
         post = {"q": ""}
     reply = requests.post(url, post)
     if reply.json():
-        print(reply.json())
+        json = reply.json()
+        print("[{}] {}".format(json['id'], json['name']))
