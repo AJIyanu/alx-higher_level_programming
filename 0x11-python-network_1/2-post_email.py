0cc3438064a1751e3ab14a2902be712ed4f9f42b
AJIyanu <aderemijoseph@live.com>
2022-09-24 02:27:57 +0100

    decode again

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,6 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     data = urllib.parse.urlencode({"email": sys.argv[2]})
-    data = data.encode("utf8")
+    data = data.encode("utf-8")
     with urllib.request.urlopen(url, data) as response:
-        print(response.read())
+        reply = response.read()
+        print(reply.decode("utf-8"))
