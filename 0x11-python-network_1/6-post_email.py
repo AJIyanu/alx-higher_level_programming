1fd1d639170aa11ec4dd9d4bf4a74fb56a1a12cf
AJIyanu <aderemijoseph@live.com>
2022-09-24 03:56:36 +0100

    testing on sandbox

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
 """My Comments come here and must be edited"""
 
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    post = {"email": sys.argv[2]}
+    reply = requests.post(url, post)
+    print(reply.text)
