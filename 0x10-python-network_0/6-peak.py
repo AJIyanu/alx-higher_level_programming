7519872989e5be52e125b521f222057dc33bcb68
AJIyanu <aderemijoseph@live.com>
2022-09-26 01:05:37 +0100

    try except

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This module contains a function that finds the peak of an 
+unsorted list.
+"""
+
+
+def find_peak(list_of_integers):
+    """This function finds the peak of an insorted list"""
+    try:
+        return (max(list_of_integers))
+    except Exception:
+        return (None)
