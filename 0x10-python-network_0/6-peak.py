9e989ad6fc285456b447d66575004ec0543e435f
AJIyanu <aderemijoseph@live.com>
2022-09-28 12:30:55 +0100

    to br continued

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,7 +7,18 @@ unsorted list.
 
 def find_peak(list_of_integers):
     """This function finds the peak of an insorted list"""
-    try:
-        return (max(list_of_integers))
-    except Exception:
+    if len(list_of_integers) == 0:
         return (None)
+    if len(list_of_integers) == 1:
+        return (list_of_integers[0])
+    if len(list_of_integers) == 2:
+        return (max(list_of_integers))
+    if list_of_integers[0] > list_of_integers [1]:
+        return (list_of_integers[0])
+    if list_of_integers[len(list_of_integers) - 1] > list_of_integers[len(list_of_integers) -2]:
+        return (list_of_integers[len(list_of_integers) -1])
+    lst = list_of_integers
+    for search in range(len(list_of_integers)):
+        end = len(list_of_integers) - 1
+        mid = (end - search) / 2
+        if list_of_integers[mid] > list_of_integers[mid]
