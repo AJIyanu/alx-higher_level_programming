cb2ca0da5e7d1bc91b727aaa658c2b17029f97a8
AJIyanu <aderemijoseph@live.com>
2022-09-28 13:47:30 +0100

    common py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,7 +14,7 @@ def find_peak(list_of_integers):
         return (list_of_integers[0])
     if len(list_of_integers) == 2:
         return (max(list_of_integers))
-    if list_of_integers[0] > list_of_integers [1]:
+    if list_of_integers[0] > list_of_integers[1]:
         return (list_of_integers[0])
     if list_of_integers[len(lst) - 1] > list_of_integers[len(lst) - 2]:
         return (list_of_integers[len(list_of_integers) - 1])
