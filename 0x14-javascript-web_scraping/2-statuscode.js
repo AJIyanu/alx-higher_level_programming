cf73dc6a958976f47e1ce14c2cd43f6a974afbae
AJIyanu <aderemijoseph@live.com>
2022-10-19 15:48:29 +0100

    check

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// send a get request to a server
+
+const arg = process.argv;
+const req = new XMLHttpRequest();
+
+req.open('GET', arg[2], false);
+req.send();
+const result = req.responseText;
+console('code: ' + result.status);
