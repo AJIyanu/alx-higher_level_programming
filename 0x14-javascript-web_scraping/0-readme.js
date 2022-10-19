1baf5970a9f66c2158e0db6a230b546d9aca25e5
AJIyanu <aderemijoseph@live.com>
2022-10-19 12:46:35 +0100

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,9 +2,10 @@
 // print out the text in utf8
 // print error if error
 
+const arg = process.argv;
 const fs = require('fs');
 
-fs.readFile('input.txt', 'utf-8', (err, data) => {
+fs.readFile(arg[1], 'utf-8', (err, data) => {
   if (err) throw err;
   console.log(data);
 });
