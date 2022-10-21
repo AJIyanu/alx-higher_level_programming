dae0c2de2b579f2607bf7499ae02e8be7dd4e2e4
AJIyanu <aderemijoseph@live.com>
2022-10-21 14:22:32 +0100

    whats the output first

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// count number of completed task
+// code written by me gangan
+
+const request = require('request');
+
+request(process.argv[2], function(error, body) {
+  if (error) {
+    console.error(error);
+  }
+  let dict = {};
+  const allbdy = body;
+  console.log(allbdy);
+}
+);
