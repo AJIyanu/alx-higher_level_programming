08177b912ded0afababd56394cb20b812662e4a2
AJIyanu <aderemijoseph@live.com>
2022-10-21 12:02:04 +0100

    something else

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,18 +1,9 @@
 #!/usr/bin/node
-// send a get request to a server
 
-const fetch = require('node-fetch')
-
-const arg = process.argv;
-
-function makeRequest () {
-  fetch(arg[2])
-    .then(response => {
-      console.log('code:', response.status);
-    })
-    .catch(err => {
-      console.log(err);
-    });
-}
-
-makeRequest();
+const request = require('request');
+request(process.argv[2], function (error, response) {
+  if (error) {
+    console.error(error);
+  }
+  console.log('code:', response && response.statusCode);
+});
