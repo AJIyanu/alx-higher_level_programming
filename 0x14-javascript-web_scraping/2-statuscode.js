60dbb8d311a5216a27b5723d596aeb929018bcce
AJIyanu <aderemijoseph@live.com>
2022-10-21 11:27:35 +0100

    another code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,7 +5,15 @@ import fetch from 'node-fetch';
 
 const arg = process.argv;
 
-fetch(arg[2])
-  .then(response => {
-    console.log('code: ' + response.status);
-  });
+function makeRequest () {
+  fetch(arg[2])
+    .then(response => {
+      console.log('response.status: ', response.status);
+      console.log(response);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+makeRequest();
