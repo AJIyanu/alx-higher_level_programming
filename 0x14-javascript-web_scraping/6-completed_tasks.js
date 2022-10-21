2dfc3d2d57f10ef7e8f70660b01a53ff385636cc
AJIyanu <aderemijoseph@live.com>
2022-10-21 14:57:37 +0100

    check

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -10,6 +10,22 @@ request(process.argv[2], function(error, response) {
   }
   let dict = {};
   const allbdy = JSON.parse(response.body);
-  console.log(allbdy[0]);
+  for (let i = 0; i < allbdy.length; i++) {
+    if (allbdy[i].userId in dict) {
+      if (allbdy[i].completed === true) {
+        const key = allbdy[i].userId;
+        dict.key = dict.key + 1;
+      }
+    else {
+      const key = allbdy[i].userId;
+      if (allbdy[i].completed === true) {
+        dict.key = 1;
+      }
+      else {
+      dict.key = 0
+    }
+    }
+}
+  console.log(dict);
 }
 );
