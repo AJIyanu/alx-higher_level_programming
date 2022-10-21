d03a2b63d60bc31a0e05223139adbdf9acf0eb12
AJIyanu <aderemijoseph@live.com>
2022-10-21 16:35:16 +0100

    what is returned?

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,26 +1,9 @@
 #!/usr/bin/node
-// again character
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request.get(url, function (err, res, body) {
-  if (err) {
-    console.log(err);
-    return;
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response) {
+  if (error) {
+    console.error(error);
   }
-  const characters = JSON.parse(body).characters;
-  const nameList = [];
-  for (let i = 0; i < characters.length; i++) {
-    request.get(characters[i], function (err, res, body) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      const name = JSON.parse(body).name;
-      nameList.push(name);
-    });
-  }
-  for (let j = 0; j < nameList.length; j++) {
-    console.log(nameList[j]);
-  }
+  console.log(JSON.parse(response.body).characters);
 });
