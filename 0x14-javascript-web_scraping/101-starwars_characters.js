1f8371a6ee5dc8491b94d05f3e893c696c265cab
AJIyanu <aderemijoseph@live.com>
2022-10-21 17:03:01 +0100

    now names

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,13 +7,12 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (erro
     console.error(error);
   }
   charlist = JSON.parse(response.body).characters;
-  console.log(charlist);
   for (let i = 0; i < charlist.length; i++) {
     request(charlist[i], function (err, resp) {
       if (err) {
         console.error(err);
       }
-      console.log(JSON.parse(resp.body));
+      console.log(JSON.parse(resp.body).name);
     });
   }
 });
