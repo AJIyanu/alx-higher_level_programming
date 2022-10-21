7673d960d3244a8554a8cb6c87b7204fc9bedc3b
AJIyanu <aderemijoseph@live.com>
2022-10-21 13:49:53 +0100

    string to dictionary then print

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,5 +5,5 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (erro
   if (error) {
     console.error(error);
   }
-  console.log(typeof (response.body));
+  console.log(JSON.parse(response.body).title);
 });
