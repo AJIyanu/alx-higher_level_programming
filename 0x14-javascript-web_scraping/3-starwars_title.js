922dc21da42bb6893bcadbf25de6d98a8c0e6026
AJIyanu <aderemijoseph@live.com>
2022-10-21 13:37:50 +0100

    trying json

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,5 +5,5 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (erro
   if (error) {
     console.error(error);
   }
-  console.log(response && response.title);
+  console.log(response && response.toJSON);
 });
