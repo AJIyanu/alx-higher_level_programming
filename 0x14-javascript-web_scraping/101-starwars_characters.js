5c6e7b270190beceb062804d6936979c68e55ae5
AJIyanu <aderemijoseph@live.com>
2022-10-22 02:22:47 +0100

    tyi877

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,32 +1,15 @@
 #!/usr/bin/node
-
 const request = require('request');
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response) {
-  if (error) {
-    console.error(error);
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (err, response, content) {
+  if (!err) {
+    const characters = JSON.parse(content).characters;
+    characters.forEach((character) => {
+      request(character, function (err, response, content) {
+        if (!err) {
+          console.log(JSON.parse(content).name);
+        }
+      });
+    });
   }
-  const charlist = JSON.parse(response.body).characters;
-  // console.log(charlist);
-  const dict = {};
-  for (const i in charlist) {
-    const url = charlist[i];
-    // console.log(url);
-    dict[url] = work(url);
-    sleep(6000);
-  }
-  console.log(dict);
 });
-
-const work = async (url) => {
-  // await sleep(1000);
-  request(url, function (err, resp) {
-    if (err) {
-      console.error(err);
-    }
-    return (JSON.parse(resp.body).name);
-  });
-};
-
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
