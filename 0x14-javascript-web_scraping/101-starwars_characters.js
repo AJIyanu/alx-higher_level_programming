08c5e35a665dcbe31573fcc987fb1a3445d20530
AJIyanu <aderemijoseph@live.com>
2022-10-22 03:35:48 +0100

    now

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,27 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
-const film = process.argv[2];
-const url = 'http://swapi-api.hbtn.io/api/films/';
-function filmcharacters (film, url) {
-  request(url, function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else if (response.statusCode === 200) {
-      let jsonobj = JSON.parse(body);
-      let people = jsonobj.results;
-      for (let i in people) {
-        for (let j in people[i].films) {
-          if (people[i].films[j].includes(film)) {
-            console.log(people[i].name);
-          }
+const url = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(url, function (err, response) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const characters = JSON.parse(response.body).characters;
+  characters.forEach((character) => {
+    setTimeout(function () {
+      request(character, function (err, response) {
+        if (!err) {
+          console.log(JSON.parse(response.body).name);
         }
-      }
-      if (jsonobj.next !== null) {
-        filmcharacters(film, jsonobj.next);
-      }
-    } else {
-      console.log('An error occured. Status code: ' + response.statusCode);
-    }
+      });
+    }, 5000);
   });
-}
-filmcharacters(film, url);
+});
