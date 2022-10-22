b76a7d0b6dba5473b1568f6288bdc3827ddaf657
AJIyanu <aderemijoseph@live.com>
2022-10-22 03:33:14 +0100

    ghfg

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
 const film = process.argv[2];
-let url = 'http://swapi.co/api/people/';
+const url = 'http://swapi-api.hbtn.io/api/films/';
 function filmcharacters (film, url) {
   request(url, function (err, response, body) {
     if (err) {
