7a18fb977f255d9d6b31650c6f0d7ca69715b325
AJIyanu <aderemijoseph@live.com>
2022-10-25 23:28:47 +0100

    whats wrong

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get('http://swapi-api.hbtn.io/api/people', function (err, response) {
   if (!err) {
-    const charlist = JSON.parse(response.body).result;
+    const charlist = JSON.parse(response.body);
     console.log(charlist);
   }
 });
