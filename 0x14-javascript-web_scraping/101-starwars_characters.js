b21317c2a0c0c434e724903f116625201225a443
AJIyanu <aderemijoseph@live.com>
2022-10-26 00:57:47 +0100

    do it

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,7 +6,7 @@ request.get(url, function (err, response) {
     const charlist = JSON.parse(response.body).characters;
     console.log(charlist);
     for (let i = 0; i < charlist.length; i++) {
-      console.log(function () {
+      console.log(() => {
         request.get(charlist[i], function (error, response) {
           if (!error) {
             return (JSON.parse(response.body).name);
