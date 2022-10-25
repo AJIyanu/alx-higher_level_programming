1ddcdc1b7b8aed984da65cd0aae7e4062891ef44
AJIyanu <aderemijoseph@live.com>
2022-10-26 00:46:28 +0100

    characters

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = 'http://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get(url, function (err, response) {
   if (!err) {
-    const charlist = JSON.parse(response.body);
+    const charlist = JSON.parse(response.body).characters;
     console.log(charlist);
   }
 });
