283a23044479457db1f2d770cd86821723cfb1d8
AJIyanu <aderemijoseph@live.com>
2022-10-26 00:17:11 +0100

    do we have all?

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -10,13 +10,14 @@ request.get('http://swapi-api.hbtn.io/api/people', function (err, response) {
     }
     console.log(chardict);
     for (let page = 2; page < 10; page++) {
-      request.get('http://swapi-api.hbtn.io/api/people', function (err, response) {
+      request.get('http://swapi-api.hbtn.io/api/people/?page=' + page, function (err, response) {
         if (!err) {
           charlist = JSON.parse(response.body).results;
           for (let i = 0; i < charlist.length; i++) {
             chardict[charlist[i].url] = charlist[i].name;
           }
         }
+        console.log(chardict);
       });
     }
   }
