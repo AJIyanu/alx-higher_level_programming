fbae30283986c73be49793197598f4d908ad1bdc
AJIyanu <aderemijoseph@live.com>
2022-11-05 06:32:13 +0100

    merge sync

--- 0x0F-python-object_relational_mapping/check.sql
+++ 0x0F-python-object_relational_mapping/check.sql
@@ -1,3 +1,13 @@
+-- Create database hbtn_0e_14_usa, tables states and cities + some data
+CREATE DATABASE IF NOT EXISTS hbtn_0e_14_usa;
+USE hbtn_0e_14_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
 
 CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT,
