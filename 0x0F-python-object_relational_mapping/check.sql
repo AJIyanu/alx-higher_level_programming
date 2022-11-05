7d605e7b59a8711bbfdfe4d1e9b50802e3c2cdfe
AJIyanu <aderemijoseph@live.com>
2022-11-04 19:08:05 -0700

    testing

--- 0x0F-python-object_relational_mapping/check.sql
+++ 0x0F-python-object_relational_mapping/check.sql
@@ -1,5 +1,2 @@
--- Create states table in hbtn_0e_4_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
-USE hbtn_0e_6_usa;
-SHOW CREATE TABLE states;
+-- Insert states
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
