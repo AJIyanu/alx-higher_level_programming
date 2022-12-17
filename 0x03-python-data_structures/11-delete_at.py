94419607d9502cf688877a1e59cc25f183cb327d
AJIyanu <aderemijoseph@live.com>
2022-12-17 02:44:08 +0100

    dara struct

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
-
 def delete_at(my_list=[], idx=0):
-    n = len(my_list)
-    if n >= 0 and idx < n:
-        for i in range(n):
-            if i == idx:
-                del(matrix[i])
-        return(my_list)
-    else:
-        return(my_list)
+    if not my_list or idx < 0:
+        return (my_list)
+    if idx < len(my_list):
+        del my_list[idx]
+    return (my_list)
